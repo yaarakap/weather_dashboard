fe4956b94d0ec01679ca2dc92513c82fdba07700
Yaara Kaplan <yaara_kaplan@brown.edu>
2026-04-30 11:41:07 -0400

    issue with text wrapping

--- weather_dash.py
+++ weather_dash.py
@@ -18,18 +18,31 @@ fig = px.scatter_map(coords, lat="latitude", lon="longitude",
 fig.update_layout(mapbox_style="open-street-map")
 fig.update_layout(margin=dict(l=10, r=10, t=10, b=10))
 
-metric_df = global_df[['country', 'location_name', 'temperature_celsius', 
-                       'condition_text', 'wind_kph',
-    'wind_direction', 'pressure_mb', 
-       'precip_mm', 'humidity', 'cloud', 'feels_like_celsius',
-       'visibility_km', 'uv_index', 'gust_kph']]
+global_df.rename(columns={"temperature_celsius": "temp (C°)", 
+                          "temperature_fahrenheit": "temp (F°)",
+                          "condition_text": "condition",
+                          "wind_kph": "wind (kph)", "wind_mph": "wind (mph)",
+                          "pressure_mb": "pressure (mb)", "pressure_in": "pressure (in)",
+                          "precip_mm": "precipitation (mm)", "precip_in": "precipitation (in)",
+                          "feels_like_celsius": "feels like (C°)",
+                          "feels_like_fahrenheit": "feels like (F°)",
+                          "visibility_km": "visibility (km)", "visibility_miles": "visibilit (mi)",
+                          "gust_kph": "gust (kph)", "gust_mph": "gust (mph)",
+                          "uv_index": "UV index"},
+                inplace=True)
+
+metric_df = global_df[['country', 'location_name', 'temp (C°)', 
+                       'condition', 'wind (kph)',
+        'wind_direction', 'pressure (mb)', 
+       'precipitation (mm)', 'humidity', 'cloud', 'feels like (C°)',
+       'visibility (km)', 'UV index', 'gust (kph)']]
 
 imp_df = global_df[['country', 'location_name',
-       'temperature_fahrenheit', 'condition_text', 'wind_mph',
-       'wind_direction', 'pressure_in',
-       'precip_in', 'humidity', 'cloud',
-       'feels_like_fahrenheit', 'visibility_miles',
-       'uv_index', 'gust_mph']
+       'temp (F°)', 'condition', 'wind (mph)',
+       'wind_direction', 'pressure (in)',
+       'precipitation (in)', 'humidity', 'cloud',
+       'feels like (F°)', 'visibilit (mi)',
+       'UV index', 'gust (mph)']
 ]
 
 
@@ -39,7 +52,8 @@ app.layout = html.Div(children=[
     dcc.RadioItems(options=[{"label": "Metric", "value": "metric_df"}, 
                             {"label": "Imperial", "value": "imp_df"}], 
                     value='metric_df', 
-                    id='controls-and-radio-item'),
+                    id='controls-and-radio-item',
+                    inline=True),
     html.Hr(),
     html.Div(
         style={"display": "flex", "flexDirection": "row", "gap": "5px"},
@@ -47,11 +61,16 @@ app.layout = html.Div(children=[
             dag.AgGrid(
                 id="grid",
                 style={"width": "60vw", "height": "95vh", "overflowX": "auto"},
-                columnDefs=[{"field": i, "minWidth": 120, "resizable": True} 
+                columnDefs=[{"field": i, "minWidth": 120, "maxWidth": 200, "resizable": True, } 
                             for i in metric_df.columns],
                 rowData=metric_df.to_dict("records"),
-                columnSize="autoSize"
-            ),
+                columnSize="autoSize",
+                dashGridOptions={"suppressColumnVirtualisation": True},
+            defaultColDef={
+                "wrapText": True,
+                "autoHeight": True,
+                "cellStyle": {"wordBreak": "normal"}
+            }),
             dcc.Graph(figure=fig, style={"width": "15vw", "height": "95vh"})
         ]
     )
@@ -73,7 +92,7 @@ def update_grid(selected_df):
     target_df = metric_df if selected_df == 'metric_df' else imp_df
     
     # Return new data and new column definitions
-    return target_df.to_dict('records'), [{"field": i, "minWidth": 120, "resizable": True} 
+    return target_df.to_dict('records'), [{"field": i, "minWidth": 20, "resizable": True} 
                             for i in target_df.columns]
 
 
